8d28c820aef06b740e3e1821790ee8fb395a126f
miracool <makurseme@gmail.com>
2023-08-05 01:34:54 +0100

    feat/set up starter solid project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import Card from "./component/Card";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div>
+      <header>
+        <h1 class="text-3xl  font-bold">
+            Hello world!
+        </h1>
+        <button class="bg-blue-500 md:bg-green-500 ...">Subscribe</button>
+        <Card/>
       </header>
     </div>
   );
---
+++ src/component/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+   return (
+      <div>
+         <h1 class="bg-red">This is card</h1>
+      </div>
+   )
+}
+
+export default Card
